554213f88d483575fa6b09556ab9e33f4c1d3342
priyank-py <ipriyank007@hotmail.com>
2019-11-16 19:12:40 +0530

    project stated

---
+++ members/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/main/')
+    return render_to_response('login.html', context_instance=RequestContext(request))
